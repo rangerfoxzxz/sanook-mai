a5d9f8d7cd1e076343909b47dcbd4bed881fced4
DESKTOP-JITV8DB\Rangerfoxz <rangerfoxz>
2023-09-12 03:21:52 +0700

    ok im try to use carousel

---
+++ src/carousel.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { Carousel } from 'flowbite-react';
+import logo from './logo.svg';
+export default function DefaultCarousel() {
+    return (
+        <div className="h-96 w-96">
+            <Carousel slideInterval={10000}>
+
+                <img
+                    alt="..."
+                    src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
+                />
+
+                <img
+                    alt="..."
+                    src={logo}
+                />
+                <img
+                    alt="..."
+                    src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
+                />
+                <img
+                    alt="..."
+                    src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
+                />
+                <img
+                    alt="..."
+                    src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
+                />
+            </Carousel>
+        </div >
+
+    )
+}
+
+
